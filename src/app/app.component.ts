82fb079bce28539b4c2569b288d81785b23d90b3
EstivnC <eacrow41@gmail.com>
2023-04-25 21:08:15 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+/*Decorador*/
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'honduras-app';
+  title = 'Contador';
+
 }
